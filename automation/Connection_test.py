3855474219e6415d99eaec5c6c7d772502ac98a6
Hemani Bass <hemanibass@outlook.com>
2026-07-31 20:41:57 -0700

    automation folder created, created vitural environment, and added first script

---
+++ automation/Connection_test.py
@@ -0,0 +1,28 @@
+from netmiko import ConnectHandler
+
+cisco_device = {
+    'device_type': 'cisco_ios',
+    'host': '192.168.0.56',
+    'username': 'admin',
+    'password': 'cisco',
+    'secret': 'cisco',
+}
+
+print("Establishing connection to the device...")
+
+try:
+    connection= ConnectHandler(**cisco_device)
+
+    output = connection.send_command('sh ip int brief')
+    print("\n####### Device Output  #######")
+    print(output)
+
+    connection.disconnect()
+    print("Connection to the device has been closed.")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+
+
